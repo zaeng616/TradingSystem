9e63f0f76f106020b28070d37573f4fdc2a33aa5
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-28 09:57:16 +0900

    add TC for GetPrice by Mock

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -14,3 +14,17 @@ TEST(TradingSystemTest, TestMockLogin) {
 	bool ret = stockerBrocker.login(id, password);
 	EXPECT_TRUE(ret);
 }
+
+TEST(TradingSystemTest, TestMockGetPrice) {
+	MockDriver mock;
+	StockerBrockerDriverInterface stockerBrocker;
+	std::string id = "id1234";
+	std::string password = "password56";
+	int code = 987654;
+	EXPECT_CALL(mock, getPrice(code)).WillRepeatedly(testing::Return(10000));
+	stockerBrocker.selectStockBrocker(mock);
+	EXPECT_TRUE(stockerBrocker.login(id, password));
+	int ret = stockerBrocker.getPrice(code);
+	EXPECT_EQ(ret, 10000);
+}
+
