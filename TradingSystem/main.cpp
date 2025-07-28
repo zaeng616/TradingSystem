643788d8fc50c6acea48146b580a02e231abd4e2
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-28 09:43:09 +0900

    add test for mock login

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "gmock/gmock.h"
+#include "test.cpp"
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -3,3 +3,14 @@
 TEST(TradingSystemTest, AlwaysTrueTC) {
 	EXPECT_EQ(1, 1);
 }
+
+TEST(TradingSystemTest, TestMockLogin) {
+	MockDriver mock;
+	StockerBrockerDriverInterface stockerBrocker;
+	std::string id = "id1234";
+	std::string password = "password56";
+	EXPECT_CALL(mock, login(id, password)).Times(1);
+	stockerBrocker.selectStockBrocker(mock);
+	bool ret = stockerBrocker.login(id, password);
+	EXPECT_TRUE(ret);
+}
