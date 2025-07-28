441e48266bbcc2fc1b9aa4d8f1ad2e3caf9fb74c
ies.cho <ies.cho@sec.com>
2025-07-28 12:54:47 +0900

    Modifed Driver return type

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -5,7 +5,7 @@ public:
 	virtual void login(std::string ID, std::string pass) = 0;
 	virtual int getPrice(std::string code) = 0;
 	virtual void buy(std::string stockPrice, int count, int price) = 0;
-	virtual bool sell(std::string code, int price, int quantity) = 0;
+	virtual void sell(std::string code, int price, int quantity) = 0;
 };
 
 class MockDriver : public Driver {
@@ -13,7 +13,7 @@ public:
 	MOCK_METHOD(void, login, (std::string ID, std::string pass), (override));
 	MOCK_METHOD(int, getPrice, (std::string code), (override));
 	MOCK_METHOD(void, buy, (std::string stockPrice, int count, int price), (override));
-	MOCK_METHOD(bool, sell, (std::string code, int price, int quantity), (override));
+	MOCK_METHOD(void, sell, (std::string code, int price, int quantity), (override));
 };
 
 class StockerBrockerDriverInterface {
