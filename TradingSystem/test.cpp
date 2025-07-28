d62a33ff39e8eaae231f3eed48ecb9aeb46ffc92
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-28 10:12:48 +0900

    refactoring Test Code

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -1,35 +1,7 @@
 ﻿#include "gmock/gmock.h"
 
-TEST(TradingSystemTest, AlwaysTrueTC) {
-	EXPECT_EQ(1, 1);
-}
-
-TEST(TradingSystemTest, TestMockLogin) {
-	MockDriver mock;
-	StockerBrockerDriverInterface stockerBrocker;
-	std::string id = "id1234";
-	std::string password = "password56";
-	EXPECT_CALL(mock, login(id, password)).Times(1);
-	stockerBrocker.selectStockBrocker(mock);
-	bool ret = stockerBrocker.login(id, password);
-	EXPECT_TRUE(ret);
-}
-
-TEST(TradingSystemTest, TestMockGetPrice) {
-	MockDriver mock;
-	StockerBrockerDriverInterface stockerBrocker;
-	std::string id = "id1234";
-	std::string password = "password56";
-	int code = 987654;
-	EXPECT_CALL(mock, getPrice(code)).WillRepeatedly(testing::Return(10000));
-	stockerBrocker.selectStockBrocker(mock);
-	EXPECT_TRUE(stockerBrocker.login(id, password));
-	int ret = stockerBrocker.getPrice(code);
-	EXPECT_EQ(ret, 10000);
-}
-
-
-TEST(TradingSystemTest, TestMockBuy) {
+class TradingFixture :public testing::Test {
+public:
 	MockDriver mock;
 	StockerBrockerDriverInterface stockerBrocker;
 	std::string id = "id1234";
@@ -37,6 +9,25 @@ TEST(TradingSystemTest, TestMockBuy) {
 	int code = 987654;
 	int price = 10000;
 	int quantity = 100;
+
+};
+
+TEST_F(TradingFixture, TestMockLogin) {
+	EXPECT_CALL(mock, login(id, password)).Times(1);
+	stockerBrocker.selectStockBrocker(mock);
+	bool ret = stockerBrocker.login(id, password);
+	EXPECT_TRUE(ret);
+}
+
+TEST_F(TradingFixture, TestMockGetPrice) {
+	EXPECT_CALL(mock, getPrice(code)).WillRepeatedly(testing::Return(price));
+	stockerBrocker.selectStockBrocker(mock);
+	EXPECT_TRUE(stockerBrocker.login(id, password));
+	int ret = stockerBrocker.getPrice(code);
+	EXPECT_EQ(ret, price);
+}
+
+TEST_F(TradingFixture, TestMockBuy) {
 	EXPECT_CALL(mock, buy(code, price, quantity)).Times(1);
 	stockerBrocker.selectStockBrocker(mock);
 	EXPECT_TRUE(stockerBrocker.login(id, password));
