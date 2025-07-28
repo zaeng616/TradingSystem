a13f279ef71266709ee18635f17bf647b39749bc
Seungah Lim <sss.lim@samsung.com>
2025-07-28 10:35:44 +0900

    Add draft code for getPrice

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -1,30 +1,59 @@
 ﻿#include "gmock/gmock.h"
 
-TEST(TradingSystemTest, AlwaysTrueTC) {
-	EXPECT_EQ(1, 1);
-}
+using namespace testing;
 
-TEST(TradingSystemTest, TestMockLogin) {
-	MockDriver mock;
-	StockerBrockerDriverInterface stockerBrocker;
-	std::string id = "id1234";
-	std::string password = "password56";
-	EXPECT_CALL(mock, login(id, password)).Times(1);
-	stockerBrocker.selectStockBrocker(mock);
-	bool ret = stockerBrocker.login(id, password);
-	EXPECT_TRUE(ret);
-}
+class Driver {
+public:
+	virtual void login(std::string ID, std::string pass) = 0;
+	virtual int getPrice(int code) = 0;
+};
 
-TEST(TradingSystemTest, TestMockGetPrice) {
+class MockDriver : public Driver {
+public:
+	MOCK_METHOD(void, login, (std::string ID, std::string pass), (override));
+	MOCK_METHOD(int, getPrice, (int code), (override));
+};
+
+class StockBrockerDriverInterface {
+public:
+	void selectStockBrocker(Driver& driver) {
+		this->driver = &driver;
+	}
+	bool login(std::string ID, std::string pass) {
+		driver->login(ID, pass);
+		return true;
+	}
+	int getPrice(int code) {
+		return 10000;
+	}
+private:
+	Driver* driver = nullptr;
+};
+
+class TradingSystemFixture : public Test {
+public:
 	MockDriver mock;
-	StockerBrockerDriverInterface stockerBrocker;
+	StockBrockerDriverInterface stockBrocker;
 	std::string id = "id1234";
 	std::string password = "password56";
 	int code = 987654;
-	EXPECT_CALL(mock, getPrice(code)).WillRepeatedly(testing::Return(10000));
-	stockerBrocker.selectStockBrocker(mock);
-	EXPECT_TRUE(stockerBrocker.login(id, password));
-	int ret = stockerBrocker.getPrice(code);
-	EXPECT_EQ(ret, 10000);
+};
+
+TEST_F(TradingSystemFixture, AlwaysTrueTC) {
+	EXPECT_EQ(1, 1);
 }
 
+TEST_F(TradingSystemFixture, TestMockLogin) {
+	EXPECT_CALL(mock, login(id, password)).Times(1);
+	stockBrocker.selectStockBrocker(mock);
+	bool ret = stockBrocker.login(id, password);
+	EXPECT_TRUE(ret);
+}
+
+TEST_F(TradingSystemFixture, TestMockGetPrice) {
+	EXPECT_CALL(mock, getPrice(code)).WillRepeatedly(testing::Return(10000));
+	stockBrocker.selectStockBrocker(mock);
+	EXPECT_TRUE(stockBrocker.login(id, password));
+	int ret = stockBrocker.getPrice(code);
+	EXPECT_EQ(ret, 10000);
+}
